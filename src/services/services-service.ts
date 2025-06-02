873143c1df61bf46c6d450f2e098666e5d759938
ahmed kotb <ahmedkotb0101@gmail.com>
2025-06-02 12:59:05 +0300

    updates

--- src/services/services-service.ts
+++ src/services/services-service.ts
@@ -4,25 +4,33 @@ import { WebError } from '../utilities/web-errors';
 
 export class ServicesService {
     async list(req: Request) {
-        const page = req.query.page ? +req.query.page : -1;
-        const limit = req.query.limit ? +req.query.limit : 10;
-        const offset = (page - 1) * limit;
-        let where: any;
+        // const page = req.query.page ? +req.query.page : -1;
+        // const limit = req.query.limit ? +req.query.limit : 10;
+        // const offset = (page - 1) * limit;
+        // let where: any;
 
-        if (page != -1) {
-            where = {
-                offset,
-                limit
-            }
-        }
+        // if (page != -1) {
+        //     where = {
+        //         offset,
+        //         limit
+        //     }
+        // }
 
-        const { count, rows } = await Service.findAndCountAll(where)
+        const { count, rows } = await Service.findAndCountAll()
         const response = {
             count,
-            activePage: page,
             rows
         }
         return response
+    }
+
+    async details(service_id: string) {
+        const id = service_id;
+        const service = await Service.findByPk(id);
+        if(!service) {
+            throw WebError.BadRequest('invalid service_id, please review.')
+        }
+        return service;
     }
 
     async create(service_type: string) {
