01f444c5102d968e1974c038f92d20b4f02089b1
AhmedKotbb <ahmedkotbb007@gmail.com>
2025-06-18 17:39:57 +0300

    update list merchant api for mobile app

--- src/services/merchants-service.ts
+++ src/services/merchants-service.ts
@@ -1,21 +1,30 @@
 import { Request } from 'express';
 import { ActiveMerchants } from "../models/merchant";
+import { Service } from '../models/services';
 
 export class MerchantsService {
     async listActiveMerchants(req: Request) {
         const page = req.query.page ? +req.query.page : -1;
         const limit = req.query.limit ? +req.query.limit : 10;
         const offset = (page - 1) * limit;
-        let where: any;
+        const includeMerchants = {
+            model: ActiveMerchants,
+            as: 'merchants',
+            attributes: ['merchant_id', 'commercial_name']
+        };
+        let opt: any = {
+            include: [includeMerchants],
+        };
 
         if (page != -1) {
-            where = {
+            opt = {
+                include: [includeMerchants],
                 offset,
                 limit
             }
         }
 
-        const { count, rows } = await ActiveMerchants.findAndCountAll(where)
+        const { count, rows } = await Service.findAndCountAll(opt)
         const response = {
             count,
             activePage: page,
